8b46c57c0823f51bbd540cbf0e2ebe65e59f50d1
giannag27 <giacosey28@gmail.com>
2025-08-14 19:01:09 -0400

    Made strawberry boba tea recipe with javascript.

---
+++ milktea.js
@@ -0,0 +1,29 @@
+// 🤍🧋🍓
+
+console.log("Strawberry Boba Tea Recipe");
+
+console.log("Ingredients:");
+console.log("1 Cup of Strawberries");
+console.log("Sugar");
+console.log("Milk of Choice");
+console.log("1 Cup of Tapioca Pearls");
+console.log("4 Cups of Water");
+console.log("1 Cup of Freshly Brewed Black/Green Tea");
+console.log("2-3 Tbs of Honey");
+console.log("1/2 Cup of Ice Cubes")
+console.log("Boba Cup and Straw! 🧋");
+
+console.log("Steps:");
+console.log("Boil the boba pearls in 4 cups of water for 20 min until chewy.");
+console.log(
+    "Drain the boba and and rinse in cold water, then add to a bowl with 1 Tbs of honey."
+);
+console.log("Brew the tea and put it in the fridge to cool.");
+console.log("Blend the strawberries with the remaining honey until smooth.");
+console.log("Add all the ingredients together in the boba cup.")
+
+/* 
+Secret Ingredient: Matcha Cold Foam
+Mix heavy cream, milk, matcha powder, and vanilla syrup. 🤍🍵
+Add it on top of your boba tea for an extra delicious treat!
+*/
